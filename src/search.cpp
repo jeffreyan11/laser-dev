ffc763f128d56cb3ad90fc03b2fcf2a65664522b
Jeffrey An <jeffreyan07@gmail.com>
2019-01-05 09:57:19 -0800

    .

--- src/search.cpp
+++ src/search.cpp
@@ -1035,7 +1035,7 @@ int PVS(Board &b, int depth, int alpha, int beta, int threadID, bool isCutNode, 
 
         // Singular extensions
         // If the TT move appears to be much better than all others, extend the move
-        if (depth >= 7 && reduction == 0 && extension == 0
+        if (depth >= 8 && reduction == 0 && extension == 0
          && m == hashed
          && abs(hashScore) < NEAR_MATE_SCORE
          && (nodeType == CUT_NODE || nodeType == PV_NODE)
@@ -1060,7 +1060,7 @@ int PVS(Board &b, int depth, int alpha, int beta, int threadID, bool isCutNode, 
                 // The window is lowered more for higher depths
                 int SEWindow = hashScore - depth;
                 // Do a reduced search for fail-low confirmation
-                int SEDepth = depth / 2;
+                int SEDepth = depth / 2 - 1;
 
                 score = -PVS(seCopy, SEDepth, -SEWindow - 1, -SEWindow, threadID, !isCutNode, ssi+1, &line);
 
